020b65983340b56b0b550ce946b67ace5c625ab1
mosjoreland <jkw.42699@gmail.com>
2023-03-29 17:00:32 -0700

    created a loop that dynamically creates all of the time block elements to help with saving any notes to their corresponding textarea element. appended all the elements that are created with the loop to the correct parent to apply the correct layout of the page. fixed the loop that sets the past, present, and future classes to work with the newly created elements. had to find the correct code to make the hourLabel text show the desired times, and used dayjs().hour(i) and applied formatting. after all this, i commented out all of the pre existing time block elements to check that my dynamically created elments worked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,56 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-  // let saveBtn = document.querySelector('.saveBtn');
-  // let note = document.querySelector('.description');
+// this will save the content to the screen after refreshing using getItem
+// outside of the function so it occurs before the other funcitons if there
+// is content to be displayed
+// figure out how to add the ids for the different timeblocks 
+  let savedNote = localStorage.getItem('savedNote');
+  if (savedNote !== null) {
+    $('#note-1').val(savedNote);
+    console.log(savedNote);
+  }
 
-  // note.addEventListener('click', event => {
-  //   localStorage.setItem('event', event.target.value);
-  // });
+  if (savedNote !== null) {
+    $('#note-2').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-3').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-4').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-5').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-6').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-7').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-8').val(savedNote)
+    console.log(savedNote);
+  }
+
+  if (savedNote !== null) {
+    $('#note-9').val(savedNote)
+    console.log(savedNote);
+  }
+
 
 $(function () {
   let currentTime = dayjs().hour();
@@ -18,22 +62,16 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  let saveBtn = $('.saveBtn');
-  // let savedNote = localStorage.getItem('savedNote');
-  // if (savedNote !== null) {
-  //   $('description').val(savedNote)
-  // }
 
-  saveBtn.click(function () {
-    let note = $('.description').val();
-    localStorage.setItem('savedNote', note);
-    
-    let savedNote = localStorage.getItem('savedNote');
-      if (savedNote !== null) {
-        $('description').val(savedNote)
-      }
 
-  });
+  // let saveBtn = $('.saveBtn');
+
+
+  // saveBtn.click(function () {
+  //   let note = $('.description').val();
+  //   localStorage.setItem('savedNote', note);
+  // });
+
 
   // saveBtn.click(funciton () {
   //   localStorage.value = $('.description').text('description')
@@ -43,10 +81,51 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  let timeBlocks = $('.time-block');
-  console.log(timeBlocks)
 
-  timeBlocks.each(function () {
+
+  // let timeBlocks = $('.time-block');
+  // console.log(timeBlocks)
+
+  // timeBlocks.each(function () {
+  //   console.log(this.id);
+  //   let blockId = parseInt(this.id.split('-')[1]);
+  //   console.log(blockId);
+
+  //   if (blockId < currentTime) {
+  //     $(this).addClass('past');
+  //   } else if (blockId === currentTime) {
+  //     $(this).addClass('present');
+  //   } else {
+  //     $(this).addClass('future');
+  //   }
+
+  // });
+  // let firstHour = 9;
+  // let numTimeBlocks = 17;
+  let containerEl = $('#container')
+  for (let i = 9; i <= 17; i++) {
+    // let hour = firstHour++;
+    let timeBlockEl = $('<div>').addClass('row time-block').attr('id', 'hour-' + i);
+    let hourLabelEl = $('<div>').addClass('col-2 col-md-1 hour text-center py3').text(dayjs().hour(i).format('hA'));
+    let textAreaEl = $('<textarea>').addClass('col-8 col-md-10 description').attr('id', 'note-' + i).attr('rows', '3');
+    let saveBtnEl = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+    let iconEl = $('<i>').addClass('fas fa-save').attr('aira-hidden', 'true');
+
+// the order of appending determines the layout provided by the styling classes added above
+    saveBtnEl.append(iconEl);
+    timeBlockEl.append(hourLabelEl);
+    timeBlockEl.append(textAreaEl);
+    timeBlockEl.append(saveBtnEl);
+    containerEl.append(timeBlockEl);
+
+    
+
+  }
+
+  // let timeBlocks = $('.time-block');
+  // console.log(timeBlocks)
+
+  $('.time-block').each(function () {
     console.log(this.id);
     let blockId = parseInt(this.id.split('-')[1]);
     console.log(blockId);
@@ -55,11 +134,14 @@ $(function () {
       $(this).addClass('past');
     } else if (blockId === currentTime) {
       $(this).addClass('present');
-    } else { 
+    } else {
       $(this).addClass('future');
     }
 
-  })
+  });
+
+  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
